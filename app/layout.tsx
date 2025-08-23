f9c162f748c9ace9c701a64ff7241ed673340910
g-Trevisan <gtrevis15@gmail.com>
2025-08-22 22:20:08 -0300

    ajuste

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased h-full overflow-hiddeiun`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased h-full overflow-hiddein`}
       >
         {children}
       </body>
